de6437dcf6f648f98ac014314014de3d7e3c4560
Alive <julovju@gmail.com>
2020-02-09 22:43:16 +0900

    commit

--- msa-zuul-server/src/main/java/com/msa_sample01/zuul/server/MsaZuulApplication.java
+++ msa-zuul-server/src/main/java/com/msa_sample01/zuul/server/MsaZuulApplication.java
@@ -3,7 +3,6 @@ package com.msa_sample01.zuul.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
