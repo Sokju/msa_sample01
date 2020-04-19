d9c5e41b24dade2ae9fe485504013bf667681633
sjuhwang <sjuhwang@sjuhwang>
2020-04-19 20:09:51 +0900

    20200419

---
+++ msa-svc-member/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO members (email, name, comment) VALUES ('james@sample01.com', 'james', 'Administrator');
+INSERT INTO members (email, name, comment) VALUES ('john@sample01.com', 'john', 'Tester');
+INSERT INTO members (email, name, comment) VALUES ('harry@sample01.com', 'harry', 'developer');
--- msa-svc-member/src/main/resources/schema.sql
+++ msa-svc-member/src/main/resources/schema.sql
@@ -6,6 +6,3 @@ CREATE TABLE members (
   comment      VARCHAR(300)
 );
 
-INSERT INTO members (email, name, comment) VALUES ('james@sample01.com', 'james', 'Administrator');
-INSERT INTO members (email, name, comment) VALUES ('john@sample01.com', 'john', 'Tester');
-INSERT INTO members (email, name, comment) VALUES ('harry@sample01.com', 'harry', 'developer');
