5b01753c2bf74b84a297f35b9efe0ab932f341c7
sjuhwang <sjuhwang@sjuhwang>
2020-04-21 00:50:46 +0900

    inmemory auth test

--- msa-auth-server/src/main/java/com/msa_sample01/auth/server/config/WebSecurityConfig.java
+++ msa-auth-server/src/main/java/com/msa_sample01/auth/server/config/WebSecurityConfig.java
@@ -1,57 +1,28 @@
 package com.msa_sample01.auth.server.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.msa_sample01.auth.server.service.MemberService;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-	
-	@Autowired
-	private MemberService userDetailService;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	 auth.authenticationProvider(authenticationProvider());
-	}
-
-   /**
-     * 데이터베이스 인증용 Provider
-     * @return
-     */
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
-    }
-    
     @Override
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
-    /*
-
+    
     @Override
     @Bean
     public UserDetailsService userDetailsServiceBean() throws Exception {
@@ -60,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-    	web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**");
+        web.ignoring().antMatchers("/resources/**");
     }
     
     @Override
@@ -80,34 +51,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     	http.csrf().disable();
     	
 		//http.httpBasic();
-    	
-//    	http.authorizeRequests()
-//        // 페이지 권한 설정
-//	        .antMatchers("/admin/**").hasRole("ADMIN")
-//	        .antMatchers("/user/myinfo").hasRole("MEMBER")
-//	        .antMatchers("/**").permitAll()
-//	    .and() // 로그인 설정
-//	        .formLogin()
-//	        .loginPage("/user/login")
-//	        .defaultSuccessUrl("/user/login/result")
-//	        .permitAll()
-//	    .and() // 로그아웃 설정
-//	        .logout()
-//	        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-//	        .logoutSuccessUrl("/user/logout/result")
-//	        .invalidateHttpSession(true)
-//	    .and()
-//	        // 403 예외처리 핸들링
-//	        .exceptionHandling().accessDeniedPage("/user/denied");
 	}
     
     
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    	
-    	
-//    	auth.userDetailsService(memberService).passwordEncoder(passwordEncoder());
-    	
+
     	PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
     	
     	auth.inMemoryAuthentication()
@@ -116,7 +65,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	        .and()
 	        .withUser("james").password(encoder.encode("password")).roles("USER", "ADMIN");
     }
-    
-*/    
 
 }
--- msa-auth-server/src/main/java/com/msa_sample01/auth/server/controller/MemberController.java
+++ msa-auth-server/src/main/java/com/msa_sample01/auth/server/controller/MemberController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
+
 	@RequestMapping(value = { "/user" }, produces = "application/json")
 	public Map<String, Object> user(OAuth2Authentication user) {
 		Map<String, Object> userInfo = new HashMap<>();
@@ -18,63 +19,5 @@ public class MemberController {
 		userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
 		return userInfo;
 	}
-	
-//	private MemberService memberService;
-//
-//    // 메인 페이지
-//    @GetMapping("/")
-//    public String index() {
-//        return "/index";
-//    }
-//
-//    // 회원가입 페이지
-//    @GetMapping("/user/signup")
-//    public String dispSignup() {
-//        return "/signup";
-//    }
-//
-//    // 회원가입 처리
-//    @PostMapping("/user/signup")
-//    public String execSignup(Member memberDto) {
-////        memberService.joinUser(memberDto);
-//
-//        return "redirect:/user/login";
-//    }
-//
-//    // 로그인 페이지
-//    @GetMapping("/user/login")
-//    public String dispLogin() {
-//        return "/login";
-//    }
-//
-//    // 로그인 결과 페이지
-//    @GetMapping("/user/login/result")
-//    public String dispLoginResult() {
-//        return "/loginSuccess";
-//    }
-//
-//    // 로그아웃 결과 페이지
-//    @GetMapping("/user/logout/result")
-//    public String dispLogout() {
-//        return "/logout";
-//    }
-//
-//    // 접근 거부 페이지
-//    @GetMapping("/user/denied")
-//    public String dispDenied() {
-//        return "/denied";
-//    }
-//
-//    // 내 정보 페이지
-//    @GetMapping("/user/info")
-//    public String dispMyInfo() {
-//        return "/myinfo";
-//    }
-//
-//    // 어드민 페이지
-//    @GetMapping("/admin")
-//    public String dispAdmin() {
-//        return "/admin";
-//    }
 
 }
