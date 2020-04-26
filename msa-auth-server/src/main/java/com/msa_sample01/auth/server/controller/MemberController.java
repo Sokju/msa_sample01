9cdf1e080a6e887d2c820e02cddfb34a879fd00a
sjuhwang <sjuhwang@sjuhwang>
2020-04-26 23:52:24 +0900

    20200426

--- msa-auth-server/src/main/java/com/msa_sample01/auth/server/controller/MemberController.java
+++ msa-auth-server/src/main/java/com/msa_sample01/auth/server/controller/MemberController.java
@@ -3,6 +3,8 @@ package com.msa_sample01.auth.server.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MemberController {
 
+	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	@RequestMapping(value = { "/user" }, produces = "application/json")
 	public Map<String, Object> user(OAuth2Authentication user) {
 		Map<String, Object> userInfo = new HashMap<>();
 		userInfo.put("user", user.getUserAuthentication().getPrincipal());
 		userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
+		
+		log.debug("MemberController.user="+user.getName());
 		return userInfo;
 	}
 
