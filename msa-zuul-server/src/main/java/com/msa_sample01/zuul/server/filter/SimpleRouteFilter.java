39bccfffd67a463eda49a36f73dd21a32d55009f
sjuhwang <sjuhwang@sjuhwang>
2020-05-31 22:02:25 +0900

    20200531

--- msa-zuul-server/src/main/java/com/msa_sample01/zuul/server/filter/SimpleRouteFilter.java
+++ msa-zuul-server/src/main/java/com/msa_sample01/zuul/server/filter/SimpleRouteFilter.java
@@ -34,16 +34,18 @@ public class SimpleRouteFilter extends ZuulFilter {
 	    RequestContext ctx = RequestContext.getCurrentContext();
 	    HttpServletRequest request = ctx.getRequest();
 
-	    log.debug(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-	    log.debug("I AM HITTING THE AUTH SERVER: " + request.getHeader("Authorization"));
+	    //log.debug(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
+	    //log.debug("I AM HITTING THE AUTH SERVER: " + request.getHeader("Authorization"));
         
         Enumeration<String> headerNames = request.getHeaderNames();
 
+        /*
         while (headerNames.hasMoreElements()) {
             String headerName = headerNames.nextElement();
             
             log.debug(headerName + " : " + request.getHeader(headerName));
         }
+        */
 
 	    return null;
 	}
