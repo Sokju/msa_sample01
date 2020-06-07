fc1e67b3f31376fcb1086f32737b6c9fcec9e530
sjuhwang <sjuhwang@sjuhwang>
2020-06-08 00:08:50 +0900

    20200606

--- platform_config/set_delete_image.sh
+++ platform_config/set_delete_image.sh
@@ -1,13 +1,19 @@
 echo $1 $2
+
+IMG_NM=$1
+TAG_NO=$2
+
 echo "---------------------------------------------------"
-curl -X GET 192.168.100.102:5000/v2/$1/tags/list
+curl -X GET 192.168.100.102:5000/v2/${IMG_NM}/tags/list
 echo "---------------------------------------------------"
 
-curl -v --silent -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X GET 192.168.100.102:5000/v2/$1/manifests/$2 2>&1 | grep Docker-Content-Digest | awk '{print ($3)}' 
+curl -v --silent -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X GET 192.168.100.102:5000/v2/${IMG_NM}/manifests/${TAG_NO} 2>&1 | grep Docker-Content-Digest | awk '{print ($3)}' 
 
-DIGEST=`curl -v --silent -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X GET 192.168.100.102:5000/v2/$1/manifests/$2 2>&1 | grep Docker-Content-Digest | awk '{print ($3)}'`
+DIGEST=`curl -v --silent -H "Accept: application/vnd.docker.distribution.manifest.v2+json" -X GET 192.168.100.102:5000/v2/${IMG_NM}/manifests/${TAG_NO} 2>&1 | grep Docker-Content-Digest | awk '{print ($3)}'`
 
-curl -X DELETE 192.168.100.102:5000/v2/$1/manifests/${DIGEST}
+URL="192.168.100.102:5000/v2/${IMG_NM}/manifests/${DIGEST}"
+URL=${URL%$'\r'}
+echo "curl -X DELETE ${URL}" |awk '{print $0}' |sh -v
 
 docker exec -it k8s-private-registry  bin/registry garbage-collect  /etc/docker/registry/config.yml 
 
